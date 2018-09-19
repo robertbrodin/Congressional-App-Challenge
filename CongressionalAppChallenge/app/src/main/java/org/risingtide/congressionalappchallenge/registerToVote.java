cd5ca575627efc5946d817d08906d0f8e604ef58
robertbrodin <43351978+robertbrodin@users.noreply.github.com>
2018-09-19 11:32:58 -0400

    Add files via upload

---
+++ CongressionalAppChallenge/app/src/main/java/org/risingtide/congressionalappchallenge/registerToVote.java
@@ -0,0 +1,12 @@
+package org.risingtide.congressionalappchallenge;
+
+import android.os.Bundle;
+
+public class registerToVote extends HomeScreenActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_vote);
+    }
+}
