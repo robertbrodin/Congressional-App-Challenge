ae7793db0e989801ce74ea353a78b455503013c2
robertbrodin <43351978+robertbrodin@users.noreply.github.com>
2018-09-22 13:08:37 -0400

    fixed issue where it was going to candidates.xml

--- CongressionalAppChallenge/app/src/main/java/org/risingtide/congressionalappchallenge/centersActivity.java
+++ CongressionalAppChallenge/app/src/main/java/org/risingtide/congressionalappchallenge/centersActivity.java
@@ -7,6 +7,7 @@ public class centersActivity extends HomeScreenActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.candidates);
+        // Was having an issue that Centers went to candidates instead, the whole time it was going to the wrong activity.
+        setContentView(R.layout.centers);
     }
 }
