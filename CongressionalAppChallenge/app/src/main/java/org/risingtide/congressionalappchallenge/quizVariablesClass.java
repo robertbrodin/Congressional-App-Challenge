deaf70cb6fd4d4ff48c572850b5de2e7a43b03db
robertbrodin <43351978+robertbrodin@users.noreply.github.com>
2018-09-22 13:51:43 -0400

    Update quizVariablesClass.java

--- CongressionalAppChallenge/app/src/main/java/org/risingtide/congressionalappchallenge/quizVariablesClass.java
+++ CongressionalAppChallenge/app/src/main/java/org/risingtide/congressionalappchallenge/quizVariablesClass.java
@@ -1,11 +1,15 @@
 package org.risingtide.congressionalappchallenge;
+import android.util.Log;
 
 public class quizVariablesClass {
 
+    static int points = 1;
 
-    public static int points = 0;
-    public static int times = 0;
-    public static String error;
-
-
+    public static void setPoints(int toAdd){
+        points = points + toAdd;
+        Log.d("points set", Integer.toString(points));
+    }
+    public static int getPoints(){
+        return points;
+    }
 }
